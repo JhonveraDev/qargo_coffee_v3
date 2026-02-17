1651397c8cac71528c619e013b52ae85b1ba6ed8
JhonveraDev <seguridad.veracontainer@gmail.com>
2026-02-17 13:31:01 -0500

    Refactor code structure for improved readability and maintainability

--- src/features/menu/data/headerBanner.data.ts
+++ src/features/menu/data/headerBanner.data.ts
@@ -1,16 +1,16 @@
 import type { CategoryId } from "../types/products.types";
 
 export const headerBanner: Record<CategoryId, string> = {
-  "hot-coffee": "banner2.png",
-  "cold-coffee": "our-menu-header-hot-coffee.png",
-  "fresh-cool": "banner2.png",
-  "tea": "banner2.png",
-  "boba-tea": "banner2.png",
-  "cold-brew": "banner2.png",
-  "energy-fresh": "banner2.png",
-  "energy-smoothies": "banner2.png",
-  "breakfast": "banner2.png",
-  "lunch-sandwiches": "banner2.png",
-  "bakery": "banner2.png",
-  "taste-of-italy": "banner2.png",
+  "hot-coffee": "our-menu-header-hot-coffee.png",
+  "cold-coffee": "our-menu-header-cold-coffee.png",
+  "fresh-cool": "our-menu-header-fresh-cool.png",
+  "tea": "our-menu-header-tea.png",
+  "boba-tea": "our-menu-header-boba-tea.png",
+  "cold-brew": "our-menu-header-cold-brew.png",
+  "energy-fresh": "our-menu-header-energy-fresh.png",
+  "energy-smoothies": "our-menu-header-energy-smoothies.png",
+  "breakfast": "our-menu-header-breakfast.png",
+  "lunch-sandwiches": "our-menu-header-lunch-sandwiches.png",
+  "bakery": "our-menu-header-bakery.png",
+  "taste-of-italy": "our-menu-header-taste-of-italy.png",
 };
