3a3e2231086ef68b5ea826573b0d4ff5c1e5e14e
JhonveraDev <seguridad.veracontainer@gmail.com>
2026-02-17 17:15:33 -0500

    Refactor code structure for improved readability and maintainability

--- src/features/menu/data/products.data.ts
+++ src/features/menu/data/products.data.ts
@@ -5,58 +5,101 @@ export const allProducts: Product[] = [
     category: "hot-coffee",
     products: [
       {
-        id: "caffe-con-leche",
-        name: "Caffe con leche",
-        thumbnail: "cafe-au-lait.png",
+        id: "cafe-au-lait",
+        name: "Café Au Lait",
+        thumbnail: "hot-coffee-cafe-au-lait.png",
         mainImage: "dummy-data.png"
       },
       {
-        id: "caffe-con-leche",
-        name: "Caffe con leche",
-        thumbnail: "product-04.png",
+        id: "caffe-americano",
+        name: "Caffè Americano",
+        thumbnail: "hot-coffee-caffe-americano.png",
         mainImage: "dummy-data.png"
       },
       {
-        id: "caffe-con-leche",
-        name: "Caffe con leche",
-        thumbnail: "product-04.png",
+        id: "caffe-latte",
+        name: "Caffè Latte",
+        thumbnail: "hot-coffee-caffe-latte.png",
         mainImage: "dummy-data.png"
       },
       {
-        id: "caffe-con-leche",
-        name: "Caffe con leche",
-        thumbnail: "product-04.png",
-        mainImage: "dummy-data.png"
-      },
-    ]
-  },
-  {
-    category: "cold-coffee",
-    products: [
-      {
-        id: "caffe-con-leche",
-        name: "Caffe con leche",
-        thumbnail: "cold-coffe-iced-americano.png",
+        id: "cappuccino",
+        name: "Cappuccino",
+        thumbnail: "hot-coffee-cappuccino.png",
         mainImage: "dummy-data.png"
       },
       {
-        id: "caffe-con-leche",
-        name: "Caffe con leche",
-        thumbnail: "product-04.png",
+        id: "chai-tea-latte",
+        name: "Chai Tea Latte",
+        thumbnail: "hot-coffee-chai-tea-latte.png",
         mainImage: "dummy-data.png"
       },
       {
-        id: "caffe-con-leche",
-        name: "Caffe con leche",
-        thumbnail: "product-04.png",
+        id: "cortado",
+        name: "Cortado",
+        thumbnail: "hot-coffee-cortado.png",
         mainImage: "dummy-data.png"
       },
       {
-        id: "caffe-con-leche",
-        name: "Caffe con leche",
-        thumbnail: "product-04.png",
+        id: "espresso-con-panna",
+        name: "Espresso Con Panna",
+        thumbnail: "hot-coffee-espresso-con-panna.png",
         mainImage: "dummy-data.png"
       },
+      {
+        id: "espresso",
+        name: "Espresso",
+        thumbnail: "hot-coffee-espresso.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "espresso-macchiato",
+        name: "Espresso Macchiato",
+        thumbnail: "hot-coffee-espresso-macchiato.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "flat-white",
+        name: "Flat White",
+        thumbnail: "hot-coffee-flat-white.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "hot-chocolate",
+        name: "Hot Chocolate",
+        thumbnail: "hot-coffee-hot-chocolate.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "latte-macchiato",
+        name: "Latte Macchiato",
+        thumbnail: "hot-coffee-latte-macchiato.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "drip-coffee",
+        name: "Drip Coffee",
+        thumbnail: "hot-coffee-drip-coffee.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "matcha-latte",
+        name: "Matcha Latte",
+        thumbnail: "hot-coffee-matcha-latte.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "mocha",
+        name: "Mocha",
+        thumbnail: "hot-coffee-mocha.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "white-mocha",
+        name: "White Mocha",
+        thumbnail: "hot-coffee-white-mocha.png",
+        mainImage: "dummy-data.png"
+      }
     ]
   }
 ];
