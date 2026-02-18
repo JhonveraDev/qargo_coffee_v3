65b535a958541f32436fdd1c60b3eb7d51e40fb4
JhonveraDev <seguridad.veracontainer@gmail.com>
2026-02-18 14:33:57 -0500

    Refactor code structure for improved readability and maintainability

--- src/features/menu/components/HeaderBanner.tsx
+++ src/features/menu/components/HeaderBanner.tsx
@@ -1,13 +1,23 @@
 import { getImage } from "../utils/products-images.utils";
 
-import { headerBanner } from "../data/headerBanner.data";
+import { headerBanner, headerBannerMobile } from "../data/headerBanner.data";
 
 import type { CategoryId } from "../types/products.types";
 
 export const HeaderBanner = ({ category }: { category: CategoryId }) => {
   return (
     <header className="our-menu__banner">
-      <img src={getImage("banners", headerBanner[category])} alt={category} />
+      <picture>
+        <source
+          media="(max-width: 720px)"
+          srcSet={getImage("banners", headerBannerMobile[category])}
+        />
+
+        <img
+          src={getImage("banners", headerBanner[category])}
+          alt={category}
+        />
+      </picture>
     </header>
   );
 };
--- src/features/menu/data/headerBanner.data.ts
+++ src/features/menu/data/headerBanner.data.ts
@@ -14,3 +14,19 @@ export const headerBanner: Record<CategoryId, string> = {
   "bakery": "our-menu-header-bakery.png",
   "taste-of-italy": "our-menu-header-taste-of-italy.png",
 };
+
+
+export const headerBannerMobile: Record<CategoryId, string> = {
+  "hot-coffee": "our-menu-header-hot-coffee-mobile.png",
+  "cold-coffee": "our-menu-header-cold-coffee-mobile.png",
+  "fresh-cool": "our-menu-header-fresh-cool-mobile.png",
+  "tea": "our-menu-header-tea-mobile.png",
+  "boba-tea": "our-menu-header-boba-tea-mobile.png",
+  "cold-brew": "our-menu-header-cold-brew-mobile.png",
+  "energy-fresh": "our-menu-header-energy-fresh-mobile.png",
+  "energy-smoothies": "our-menu-header-energy-smoothies-mobile.png",
+  "breakfast": "our-menu-header-breakfast-mobile.png",
+  "lunch-sandwiches": "our-menu-header-lunch-sandwiches-mobile.png",
+  "bakery": "our-menu-header-bakery-mobile.png",
+  "taste-of-italy": "our-menu-header-taste-of-italy-mobile.png",
+};
