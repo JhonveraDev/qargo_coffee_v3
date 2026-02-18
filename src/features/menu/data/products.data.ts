567912b646bf9de9ed4575643c8abc4354256fe2
JhonveraDev <seguridad.veracontainer@gmail.com>
2026-02-17 19:40:12 -0500

    Refactor code structure for improved readability and maintainability

--- src/features/menu/data/products.data.ts
+++ src/features/menu/data/products.data.ts
@@ -101,5 +101,329 @@ export const allProducts: Product[] = [
         mainImage: "dummy-data.png"
       }
     ]
-  }
+  },
+  {
+    category: "cold-coffee",
+    products: [
+      {
+        id: "iced-americano",
+        name: "Iced Americano",
+        thumbnail: "cold-coffee-iced-americano.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "iced-cappuccino",
+        name: "Iced Cappuccino",
+        thumbnail: "cold-coffee-iced-cappuccino.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "iced-chai-latte",
+        name: "Iced Chai Latte",
+        thumbnail: "cold-coffee-iced-chai-latte.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "iced-coffee",
+        name: "Iced Coffee",
+        thumbnail: "cold-coffee-iced-coffee.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "iced-latte",
+        name: "Iced Latte",
+        thumbnail: "cold-coffee-iced-latte.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "iced-latte-macchiato",
+        name: "Iced Latte Macchiato",
+        thumbnail: "cold-coffee-iced-latte-macchiato.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "iced-matcha-latte",
+        name: "Iced Matcha Latte",
+        thumbnail: "cold-coffee-iced-matcha-latte.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "iced-mocha",
+        name: "Iced Mocha",
+        thumbnail: "cold-coffee-iced-mocha.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "iced-white-mocha",
+        name: "Iced White Mocha",
+        thumbnail: "cold-coffee-iced-white-mocha.png",
+        mainImage: "dummy-data.png"
+      }
+    ]
+  },
+  {
+    category: "fresh-cool",
+    products: [
+      {
+        id: "italian-coffee-frappe",
+        name: "Italian Coffee Frappe",
+        thumbnail: "fresh-cool-italian-coffee-frappe.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "italian-mocha-frappe",
+        name: "Italian Mocha Frappe",
+        thumbnail: "fresh-cool-italian-mocha-frappe.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "italian-chocolate-frappe",
+        name: "Italian Chocolate Frappe",
+        thumbnail: "fresh-cool-italian-chocolate-frappe.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "italian-strawberry-frappe",
+        name: "Italian Strawberry Frappe",
+        thumbnail: "fresh-cool-italian-strawberry-frappe.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "crema-di-caffe",
+        name: "Crema Di Caffè",
+        thumbnail: "fresh-cool-crema-di-caffe.png",
+        mainImage: "dummy-data.png"
+      }
+    ]
+  },
+  {
+    category: "tea",
+    products: [
+      {
+        id: "hot-tea",
+        name: "Hot Tea",
+        thumbnail: "tea-hot-tea.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "iced-tea",
+        name: "Iced Tea",
+        thumbnail: "tea-iced-tea.png",
+        mainImage: "dummy-data.png"
+      },
+    ]
+  },
+  {
+    category: "boba-tea",
+    products: [
+      {
+        id: "coco-nutty",
+        name: "CocoNutty",
+        thumbnail: "boba-tea-coco-nutty.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "espresso-rose",
+        name: "Espresso Rose",
+        thumbnail: "boba-tea-espresso-rose.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "pure-matcha",
+        name: "Pure Matcha",
+        thumbnail: "boba-tea-pure-matcha.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "mangoberry",
+        name: "Mangoberry",
+        thumbnail: "boba-tea-mangoberry.png",
+        mainImage: "dummy-data.png"
+      }
+    ]
+  },
+  {
+    category: "cold-brew",
+    products: [
+      {
+        id: "cold-brew",
+        name: "Cold Brew",
+        thumbnail: "cold-brew-cold-brew.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "nitro-cold-brew",
+        name: "Nitro Cold Brew",
+        thumbnail: "cold-brew-nitro-cold-brew.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "nutty-nitro-cold-brew",
+        name: "Nutty Nitro Cold Brew",
+        thumbnail: "cold-brew-nutty-nitro-cold-brew.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "coco-choc-nitro-cold-brew",
+        name: "Coco Choc Nitro Cold Brew",
+        thumbnail: "cold-brew-coco-choc-nitro-cold-brew.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "black-cherry-cold-brew",
+        name: "Black Cherry Cold Brew",
+        thumbnail: "cold-brew-black-cherry-cold-brew.png",
+        mainImage: "dummy-data.png"
+      }
+    ]
+  },
+  {
+    category: "energy-fresh",
+    products: [
+      {
+        id: "mother-of-dragons",
+        name: "Mother Of Dragons",
+        thumbnail: "energy-fresh-mother-of-dragons.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "mango-colada",
+        name: "Mango Colada",
+        thumbnail: "energy-fresh-mango-colada.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "watermelon-cooler",
+        name: "Watermelon Cooler",
+        thumbnail: "energy-fresh-watermelon-cooler.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "strawberries-and-cream",
+        name: "Strawberries And Cream",
+        thumbnail: "energy-fresh-strawberries-and-cream.png",
+        mainImage: "dummy-data.png"
+      }
+    ]
+  },
+  {
+    category: "energy-smoothies",
+    products: [
+      {
+        id: "mango-dreamsicle",
+        name: "Mango Dreamsicle",
+        thumbnail: "energy-smoothies-mango-dreamsicle.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "berry-burst-fusion",
+        name: "Berry Burst Fusion",
+        thumbnail: "energy-smoothies-berry-burst-fusion.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "pina-coco-lada",
+        name: "Pina Coco-lada",
+        thumbnail: "energy-smoothies-pina-coco-lada.png",
+        mainImage: "dummy-data.png"
+      }
+    ]
+  },
+  {
+    category: "breakfast",
+    products: [
+      {
+        id: "italian-rustic",
+        name: "Italian Rustic",
+        thumbnail: "breakfast-italian-rustic.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "veggie-caprese",
+        name: "Veggie Caprese",
+        thumbnail: "breakfast-veggie-caprese.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "cheddar-crisp",
+        name: "Cheddar & Crisp",
+        thumbnail: "breakfast-cheddar-crisp.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "qargo-classic",
+        name: "Qargo Classic",
+        thumbnail: "breakfast-qargo-classic.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "salmone-mattina",
+        name: "Salmone Mattina",
+        thumbnail: "breakfast-salmone-mattina.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "prosciutto-royale",
+        name: "Prosciutto Royale",
+        thumbnail: "breakfast-prosciutto-royale.png",
+        mainImage: "dummy-data.png"
+      },
+      {
+        id: "caprese-al-croissant",
+        name: "Caprese Al Croissant",
+        thumbnail: "breakfast-caprese-al-croissant.png",
+        mainImage: "dummy-data.png"
+      }
+    ]
+  },
+  {
+  category: "lunch-sandwiches",
+  products: [
+    {
+      id: "pesto-pomodoro",
+      name: "Pesto Pomodoro",
+      thumbnail: "lunch-pesto-pomodoro.png",
+      mainImage: "dummy-data.png"
+    },
+    {
+      id: "turkey-bacon-classico",
+      name: "Turkey Bacon Classico",
+      thumbnail: "lunch-turkey-bacon-classico.png",
+      mainImage: "dummy-data.png"
+    },
+    {
+      id: "turkey-bacon-al-fresco",
+      name: "Turkey Bacon Al Fresco",
+      thumbnail: "lunch-turkey-bacon-al-fresco.png",
+      mainImage: "dummy-data.png"
+    },
+    {
+      id: "turkey-bacon-formaggio",
+      name: "Turkey Bacon Formaggio",
+      thumbnail: "lunch-turkey-bacon-formaggio.png",
+      mainImage: "dummy-data.png"
+    },
+    {
+      id: "salmone-fresco",
+      name: "Salmone Fresco",
+      thumbnail: "lunch-salmone-fresco.png",
+      mainImage: "dummy-data.png"
+    },
+    {
+      id: "pinsa-di-pollo",
+      name: "Pinsa Di Pollo",
+      thumbnail: "lunch-pinsa-di-pollo.png",
+      mainImage: "dummy-data.png"
+    },
+    {
+      id: "pinsa-perfetta",
+      name: "Pinsa Perfetta",
+      thumbnail: "lunch-pinsa-perfetta.png",
+      mainImage: "dummy-data.png"
+    }
+  ]
+}
+
+
+
 ];
